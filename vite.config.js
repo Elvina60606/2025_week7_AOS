74faed6d244b14f6b3b0ebe1433a3aed04efa3ec
Hong Charlene <charlene@MacBookAir.hitronhub.home>
2025-08-17 16:20:37 +0800

    更改名稱

--- vite.config.js
+++ vite.config.js
@@ -25,7 +25,7 @@ function moveOutputPlugin() {
 export default defineConfig({
   // base 的寫法:
   // base: '/Repository 的名稱/'
-  base: '/2025_week3-vite2/',
+  base: '/2025_week7_AOS/',
   plugins: [
     liveReload(['./layout/**/*.ejs', './pages/**/*.ejs', './pages/**/*.html']),
     ViteEjsPlugin(),
